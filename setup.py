5a5647395395f4153ad9a0912bb2e776ddf28607
XiaochenCui <jcnlcxc.new@gmail.com>
2017-12-12 09:15:08 +0800

    v0.0.3

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
 
 setup(
     name='cxc-django',
-    version='0.0.1',
+    version='0.0.3',
     description='A django wrapper made by Xiaochen Cui',
+    long_description=long_description,
     url='https://github.com/XiaochenCui/cxc-django',
-    auther='Xiaochen Cui',
-    auther_email='jcnlcxc.new@gmail.com',
+    author='Xiaochen Cui',
+    author_email='jcnlcxc.new@gmail.com',
     license='GPLv3',
     classifiers=[
         'Development Status :: 1 - Planning',
